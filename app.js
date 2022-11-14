d78930158280c1c9286405f90a689ebfe487d4d0
PeterHsiao <peter12218@gmail.com>
2022-11-14 17:37:21 +0800

    maintain state -> (session)

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const app = express();
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cookieSession = require("cookie-session");
+const session = require("express-session");
 const { connect } = require("http2");
 const authRoute = require("./routes/auth-route");
-const profileRoute = require("./routes/profile")
+const profileRoute = require("./routes/profile");
+const passport = require("passport");
 require("./config/passport") // -> will import middleware into this app.js from passport.js
 
 mongoose
@@ -29,6 +32,18 @@ mongoose
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(cookieSession({
+//   keys: [process.env.SECRET]
+// }));
+// this version passport must use "express-session" but not "cookie-session"
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/auth", authRoute);  // (Route with parameter) -> https://blog.gtwang.org/programming/learn-to-use-the-new-router-in-expressjs-4/
 app.use("/profile", profileRoute);
 
--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,20 @@
+const { now } = require("mongoose");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
 const User = require("../models/user-model");
+
+passport.serializeUser((user, done) => {
+    console.log("id has been serialized.");
+    done(null, user._id); // Only user id need to be serialized.
+})
+
+passport.deserializeUser((_id, done) => {
+    console.log("Deserializing now.");
+    User.findById({_id}).then((user) => {
+        console.log("Found user.");
+        done(null, user);
+    });
+})
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
@@ -8,7 +22,7 @@ passport.use(new GoogleStrategy({
     callbackURL: "/auth/google/redirect"
 }, (accessToken, refreshToken, profile, done) => {
     // passport callback -> https://www.passportjs.org/packages/passport-google-oauth20/
-    console.log(profile);
+    // console.log(profile);
     User.findOne({googleID: profile.id}).then((foundUser) => {
         if(foundUser) {
             console.log("User already exist.");
